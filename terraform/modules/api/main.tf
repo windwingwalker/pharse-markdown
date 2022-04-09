69a90fc42395f1f62111dff7d8f1c7d0d9df556a
windWingWalker <elliot562k@gmail.com>
2022-04-09 20:57:05 +0800

    Fix api config

--- terraform/modules/api/main.tf
+++ terraform/modules/api/main.tf
@@ -30,31 +30,3 @@ resource "aws_api_gateway_method_response" "get_200" {
   http_method = aws_api_gateway_method.get.http_method
   status_code = 200
 }
-
-resource "aws_api_gateway_deployment" "default" {
-  rest_api_id = var.api_id
-
-  triggers = {
-    # NOTE: The configuration below will satisfy ordering considerations,
-    #       but not pick up all future REST API changes. More advanced patterns
-    #       are possible, such as using the filesha1() function against the
-    #       Terraform configuration file(s) or removing the .id references to
-    #       calculate a hash against whole resources. Be aware that using whole
-    #       resources will show a difference after the initial implementation.
-    #       It will stabilize to only change when resources change afterwards.
-    redeployment = sha1(jsonencode([
-      aws_api_gateway_method.get.id,
-      aws_api_gateway_integration.get.id,
-    ]))
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_api_gateway_stage" "default" {
-  deployment_id = aws_api_gateway_deployment.default.id
-  rest_api_id   = var.api_id
-  stage_name    = "prod"
-}

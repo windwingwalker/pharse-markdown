b205e37364e70b00760b520fbc27559c62ffd1f2
windWingWalker <elliot562k@gmail.com>
2022-04-10 07:31:05 +0800

    Change http method

--- terraform/modules/api/main.tf
+++ terraform/modules/api/main.tf
@@ -1,5 +1,5 @@
 resource "aws_api_gateway_method" "get" {
-  http_method   = "GET"
+  http_method   = "POST"
   authorization = "NONE"
   resource_id   = var.api_resource_id
   rest_api_id   = var.api_id
